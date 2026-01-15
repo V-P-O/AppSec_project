12b1bccc6788e7391ef285fd4195eb8cc71ba1c6
Zvonimir Pipić <zvonimir.pipic@gmail.com>
2026-01-15 16:21:35 +0100

    docker compose

--- flask/app/config.py
+++ flask/app/config.py
@@ -7,10 +7,11 @@ class Config:
     SESSION_COOKIE_SAMESITE = "Strict"
     PERMANENT_SESSION_LIFETIME = timedelta(days=30)
 
-    DB_HOST = "127.0.0.1"
+    DB_HOST = "postgres"
     DB_NAME = "app_sec"
     DB_USER = "postgres"
     DB_PASSWORD = "postgres"
+    DB_PORT = 5432
 
     SMTP_EMAIL = os.getenv("SMTP_EMAIL")
     SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
